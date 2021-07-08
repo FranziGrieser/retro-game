c76c3c587fbc21fd8bd36e105bd2b06198b5a632
Franzi Grieser <franziska@shipcloud.io>
2021-07-08 19:01:52 +0200

    WIP conditional for game logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No number!';
+  } else if (secretNumber === guess) {
+    document.querySelector('.message').textContent = '🥳 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
