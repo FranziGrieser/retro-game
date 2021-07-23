d89188f023ed41d6c8362cb85a5d97c83c0f2fb7
Franzi Grieser <franziska@shipcloud.io>
2021-07-23 10:25:06 +0200

    Implement 'again' btn eventListener Implement setting the highscore

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,8 +12,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🛑 No number!';
   } else if (secretNumber === guess) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -34,3 +40,14 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('body').style.removeProperty('background-color');
+  document.querySelector('.number').style.removeProperty('width');
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+})
